1342bb2415aa6234212dba02e3c4cbbdcab510a2
KirbCobain <66125169+KirbCobain@users.noreply.github.com>
2021-11-20 20:18:23 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*Aufgabe 1A
+        Scanner scanner = new Scanner(System.in);
+    String test = scanner.nextLine();
+        System.out.println(test.length());
+       */
+
+        /* Aufgabe 1C
+        Scanner scanner = new Scanner(System.in);
+        String test2 = scanner.nextLine();
+
+        System.out.println(test2.charAt(test2.length() -1));
+        System.out.println(test2.charAt(test2.length()));
+        */
+        /*Aufgabe 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gebe den ersten String an");
+        String test3 = scanner.nextLine();
+        System.out.println("Gebe den zweiten String an");
+        String test4 = scanner.nextLine();
+
+        System.out.println(test3 + test4);
+*/
+       /*Aufgabe 3
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gebe den ersten Namen ein");
+        String test5 = scanner.nextLine();
+        System.out.println("Gebe den zweiten Namen ein");
+        String test6 = scanner.nextLine();
+
+        System.out.println(test5.compareTo(test6));
+*/
+        //Aufgabe 4
+        
+
+
+        }
+
+}
